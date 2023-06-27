e62529c4dcc5ec38a07af11a94bd0f09108e7275
thuyng299 <131665457+thuyng299@users.noreply.github.com>
2023-06-27 14:46:33 +0700

    Add in annotation in test methods

--- src/test/java/com/nonit/personalproject/serviceimpl/IncomingDetailServiceImplTest.java
+++ src/test/java/com/nonit/personalproject/serviceimpl/IncomingDetailServiceImplTest.java
@@ -1,10 +1,20 @@
 package com.nonit.personalproject.serviceimpl;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class IncomingDetailServiceImplTest {
+    @Autowired
+    IncomingDetailServiceImpl incomingDetailServiceImpl;
 
     @Test
     void getTotalStockAmountOfRawMaterial() {
